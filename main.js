64c5ead1c457868e0b3fa400a0a4799f513eadd9
COMVIVA\ishita.singh <ishita.singh@mahindracomviva.com>
2019-05-30 13:18:28 +0530

    ishita.singh|todo list with localStorage

---
+++ main.js
@@ -0,0 +1,52 @@
+"use strict"
+function get_list(){
+    let todo=new Array;
+    let todoStr=localStorage.getItem('todo');
+    if(todoStr != null){
+        todo=JSON.parse(todoStr);
+    }
+    //return list as array
+    return todo;
+}
+
+function showList(){
+    let list = get_list();
+    let disp = '<ul>';
+    for(let i=0;i<list.length;i++){
+        disp+='<li>'+list[i]['task']+" "+list[i]['label']+'<button class="remove" id="'+i+'">X</button></li>';
+    }
+    disp+='</ul>';
+    document.getElementById('list').innerHTML=disp;
+    let buttons = document.getElementsByClassName('remove');
+    for(let i=0;i<buttons.length;i++){
+        buttons[i].addEventListener('click',remove);
+    }
+}
+
+function remove(){
+    let removeButtonId=this.getAttribute('id');
+    let list = get_list();
+    // remove 1 element from index removeButtonId
+    list.splice(removeButtonId,1);
+    let tempList = JSON.stringify(list);
+    localStorage.setItem('todo',tempList);
+    showList();
+    //check
+    //return false;
+}
+
+function addTask(){
+    let task=document.getElementById('addTask').value;
+    let label = document.getElementById('label').value;
+    let list = get_list();
+    list.push({task:task,label:label});
+    let tempList=JSON.stringify(list);
+    localStorage.setItem('todo',tempList);
+    //to refreash the list with updated items
+    showList();
+    //check use
+    //return false;
+}
+
+document.getElementById('add').addEventListener('click',addTask);
+showList();
